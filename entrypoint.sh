e9649d2e253e87ec21609f834b2389e7df260b64
Sengly456 <senglyou84@gmail.com>
2026-02-03 10:15:33 +0700

    remove -e shorthand from the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 echo "Starting Hydra..."
 echo "DSN is set: $([ -n "$DSN" ] && echo 'yes' || echo 'no')"
 echo "RAILWAY_PUBLIC_DOMAIN: ${RAILWAY_PUBLIC_DOMAIN}"
-echo "DSN format: ${DSN%%:*}"
 
 # Ensure DSN is set
 if [ -z "$DSN" ]; then
@@ -17,6 +16,6 @@ fi
 echo "Running database migrations..."
 hydra migrate sql "$DSN" --yes
 
-# Start Hydra server - use -e flag to read DSN from environment
+# Start Hydra server - config file will read ${DSN} from environment
 echo "Starting Hydra server..."
-exec hydra serve all -e --config /etc/config/hydra.yml
+exec hydra serve all --config /etc/config/hydra.yml
